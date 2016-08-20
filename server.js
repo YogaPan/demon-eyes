de94cf14cdbfe15a8f3460d98a90068880b3bdf8
YogaPan <yogapan85321@gmail.com>
2016-08-21 03:59:24 +0800

    change port

--- server.js
+++ server.js
@@ -97,13 +97,7 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-let port;
-
-if (process.env.NODE_ENV === 'production') {
-  port = 80;
-} else {
-  port = 8080;
-}
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server start listening on port ${port}`);
